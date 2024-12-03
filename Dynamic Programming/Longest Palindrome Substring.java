0ed4ede806361951f44ee2ab7d1d00107b59363e
Nikhil-Prabhat <62531542+Nikhil-Prabhat@users.noreply.github.com>
2024-12-03 18:58:01 +0530

    Update Longest Palindrome Substring.java

--- Dynamic Programming/Longest Palindrome Substring.java
+++ Dynamic Programming/Longest Palindrome Substring.java
@@ -25,3 +25,34 @@ class Solution {
         return true;
     }
 }
+
+// Accepted Solution
+class Solution {
+    public String longestPalindrome(String s) {
+        int startPalindromeIndex = 0;
+        int endPalindromeIndex = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int oddLengthPalindrome = checkPalindrome(i, i, s);
+            int evenLengthPalindrome = checkPalindrome(i, i + 1, s);
+
+            int actualLengthOfPalindrome = Math.max(oddLengthPalindrome, evenLengthPalindrome);
+            if (actualLengthOfPalindrome > endPalindromeIndex - startPalindromeIndex) {
+                startPalindromeIndex = i - (actualLengthOfPalindrome - 1) / 2;
+                endPalindromeIndex = i + (actualLengthOfPalindrome) / 2;
+            }
+        }
+
+        return s.substring(startPalindromeIndex, ++endPalindromeIndex);
+
+    }
+
+    public int checkPalindrome(int left, int right, String str) {
+        while (left >= 0 && right < str.length() && str.charAt(left) == str.charAt(right)) {
+            left--;
+            right++;
+        }
+
+        return right - left - 1;
+    }
+}
